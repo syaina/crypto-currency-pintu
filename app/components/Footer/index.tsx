63cf9c4e811dd025941121d0f9356709a58e94d5
Syaina Nur <syainanurf@gmail.com>
2024-11-22 22:33:39 +0700

    style: z-index footer

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -1,15 +1,15 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="fixed bottom-0 bg-black w-full ">
+    <footer className="fixed bottom-0 bg-black w-full z-50">
       <div className="w-full max-w-[1280px] py-6 px-[4rem] mx-auto text-white flex justify-between items-center">
         <p className="font-semibold text-base">
           Nikmati mudahnya trading pro dengan likuiditas tinggi dan biaya
           rendah, langsung dari desktop kamu.
         </p>
         <Link
-          href={'https://pintu.co.id/pro/id/markets'}
+          href={"https://pintu.co.id/pro/id/markets"}
           className="py-2 px-10 min-w-[202px] text-sm bg-[rgb(15,123,255)] font-semibold rounded-lg"
         >
           Tranding di Pintu Pro Web
