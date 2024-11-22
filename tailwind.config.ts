1e1a0f402030777aa50ec1df638a817a130213f9
Syaina Nur <syainanurf@gmail.com>
2024-11-22 10:10:01 +0700

    chore: boilerplate

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        'dark-grey': 'rgb(146, 147, 150)',
       },
+    },
+    fontFamily: {
+      primary: ['Inter', 'system-ui', 'sans-serif'],
+      secondary: ['Neue Haas Grotesk Display Pro', 'system-ui', 'sans-serif'],
     },
   },
   plugins: [],
