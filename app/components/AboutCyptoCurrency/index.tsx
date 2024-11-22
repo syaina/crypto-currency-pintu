61c723b650e73be3f32d4293e2b3fdeb31b96452
Syaina Nur <syainanurf@gmail.com>
2024-11-22 22:34:49 +0700

    style: quote mark

--- app/components/AboutCyptoCurrency/index.tsx
+++ app/components/AboutCyptoCurrency/index.tsx
@@ -11,7 +11,7 @@ const AboutCryptocurrency: React.FC = () => {
           blockchain. Terdapat 2 jenis crypto, yaitu coin dan token. Coin adalah
           cryptocurrency yang berdiri diatas blockchain-nya sendiri dengan
           contoh bitcoin dan ethereum, sedangkan token adalah cryptocurrency
-          yang “menumpang” diatas blockchain dari native aset lain.
+          yang "menumpang" diatas blockchain dari native aset lain.
         </p>
         <a
           href="https://pintu.co.id/academy/post/apa-itu-cryptocurrency"
