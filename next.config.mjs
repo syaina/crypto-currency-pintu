39714f7173d442afaeb14a5ddace0fdb33800dfd
Syaina Nur <syainanurf@gmail.com>
2024-11-22 10:10:01 +0700

    chore: boilerplate

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+// Helper to resolve paths
+path.dirname(fileURLToPath(import.meta.url));
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
